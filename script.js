0341221409044d02339bd29cd2bf76f75a31c53d
Dmitriy-Zelentsov <zelentsov.dmitry@gmail.com>
2023-10-05 12:31:18 +0300

    ADD HW1

--- script.js
+++ script.js
@@ -3,15 +3,10 @@
 // Формула перевода градусов Цельсия в градусы Фаренгейта: градусы Фаренгейта = (9 / 5) * градусы Цельсия + 32
 // Вывести в консоль температуру в Цельсиях и Фаренгейтах.
 
-// Задание 2
-// Необходимо создать переменную name, записать в эту переменную свое имя. Необходимо также создать переменную admin и присвоить этой переменной значение из переменной name. Вывести значение переменной admin в консоль.
 
-
-let temperatureCelsius = 28;
-console.log(`Температура по Цельсию ${temperatureCelsius} град.`);
-let temperatureFahrenheit = 9 / 5 * temperatureCelsius + 32;
-console.log(`Температура по Фаренгейту ${temperatureFahrenheit} град.`)
-
-const firstName = 'Dmitry';
-let admin = firstName;
-console.log(admin);
+function calcDegrees() {
+    let degrees = (document.getElementById('degrees').value);
+    let result = (9 / 5) * degrees + 32;
+    console.log(result);
+    alert(result);
+  }  
